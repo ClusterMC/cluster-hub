91c926c4e905414585b3b254938fa4c4fa2883f8
Richmond Steele <not2excel@gmail.com>
2015-10-29 16:16:20 -1000

    there, thats what i did, caveat is with mutable state objects as a setting

--- src/main/scala/org/theclustermc/hub/player/Settings.scala
+++ src/main/scala/org/theclustermc/hub/player/Settings.scala
@@ -1,8 +1,6 @@
 package org.theclustermc.hub.player
 
-import org.theclustermc.hub.player.Setting.Settingg
-
-import scala.collection.mutable
+import scala.collection.mutable.ArrayBuffer
 
 /*
  * Copyright (C) 2013-Current Carter Gale (Ktar5) <buildfresh@gmail.com>
@@ -13,13 +11,35 @@ import scala.collection.mutable
  * permission of the aforementioned owner.
  */
 
-class Settings {
-  private var settings: mutable.HashMap[Setting, SettingValue] = null
+object Settings {
 
-  def this() {
-    this()
-    settings = new mutable.HashMap[Settingg , SettingValue]
-    settings.+=(Setting.PVP, null)
-  }
+    sealed abstract class Setting[T](val name: String, private val default: T) {
+        private val _default: Option[T] = Option.apply(default)
+        private var _value: Option[T] = None
+
+        def value = _value match {
+            case x: Some[_] => _value
+            case None => _default
+        }
+
+        def value_=(x: T): Unit = _value = Option.apply(x)
+
+        def shouldSave = value.ne(_default)
+
+        def print() = {
+            println("==========")
+            println(name)
+            println(s"Default: ${_default.get}")
+            println(s"Value: ${value.get}")
+            println(s"Should Save: $shouldSave")
+            println("==========")
+        }
+
+
+    }
+
+    case class Flying() extends Setting("flying", true)
+
+    case class Numbers() extends Setting[ArrayBuffer[Int]]("numbers", ArrayBuffer(1, 2, 3))
 
 }
