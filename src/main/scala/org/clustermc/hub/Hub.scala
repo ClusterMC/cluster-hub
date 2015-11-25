1a3ecb45f71369457c9766c27c6da5b1aa789e35
Carter Gale <buildfresh@gmail.com>
2015-11-24 16:27:38 -0800

    Idek

--- src/main/scala/org/clustermc/hub/Hub.scala
+++ src/main/scala/org/clustermc/hub/Hub.scala
@@ -1,9 +1,10 @@
 package org.clustermc.hub
 
+import org.bukkit.Bukkit
 import org.bukkit.plugin.java.JavaPlugin
 import org.clustermc.hub.features.lottery.{ClusterLottery, ShardLottery}
 import org.clustermc.hub.player.HubPlayer
-import org.clustermc.lib.utils.CustomConfig
+import org.clustermc.lib.ClusterLib
 
 /*
  * Copyright (C) 2013-Current Carter Gale (Ktar5) <buildfresh@gmail.com>
@@ -15,13 +16,10 @@ import org.clustermc.lib.utils.CustomConfig
  */
 
 class Hub extends JavaPlugin {
-
-    private var _messages: Messages = null
-    def msg = _messages
-
     override def onEnable(): Unit = {
         Hub._instance = this
-        _messages = new Messages(new CustomConfig(this.getDataFolder, "lang"))
+        Bukkit.getServer.getPluginManager.getPlugin("ClusterLib")
+        .asInstanceOf[ClusterLib].assign[HubPlayer]()
         ClusterLottery.startNew()
         ShardLottery.startNew()
     }
